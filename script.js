02b57178ec7fc16f034ebb411e448435dcf98417
sumitrathore <114568634+devil9931@users.noreply.github.com>
2025-08-31 21:00:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function Car(make, model) {
 	this.make = make;
 	this.model = model;
 }
+Car.prototype.getMakeModel = function () {
+	return `${this.make} $ {this.model}`;
+}
 
 function SportsCar(make, model, topSpeed) {
 	Car.call(this,make, model);
